a1cadf7eb673217f633a4866e9136b43dc8f0f3f
Alex <81312945+VivaciousViolin@users.noreply.github.com>
2021-04-12 23:02:43 -0400

    Update time.py

--- time.py
+++ time.py
@@ -5,13 +5,19 @@ from time import sleep
 import subprocess
 from playsound import playsound
 
+input = input("press enter to start")
+
 def run():
     rantime = random.randint(1, 2)*2
     print("what time will it be??")
     sleep(rantime*60)
     print("yay")
     playsound('ding.mp3')
+    continueInput = input("press enter to start")
+    print(continueInput)
 
+run()
+run()
 run()
 run()
 run()
